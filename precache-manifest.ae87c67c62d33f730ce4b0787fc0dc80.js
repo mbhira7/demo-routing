4f751f4b32f6b572f95493e00c1422b2beace076
mbhira7 <54266598+mbhira7@users.noreply.github.com>
2019-11-27 11:23:10 +0100

    Updates

---
+++ precache-manifest.ae87c67c62d33f730ce4b0787fc0dc80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b608bab5eb2aa6842782cb105ef617",
+    "url": "/demo-routing/index.html"
+  },
+  {
+    "revision": "a0377aa6a6eb2feb7c10",
+    "url": "/demo-routing/static/css/main.2e87f52b.chunk.css"
+  },
+  {
+    "revision": "384a3169b316ed6658ef",
+    "url": "/demo-routing/static/js/2.4d2d0ff7.chunk.js"
+  },
+  {
+    "revision": "a0377aa6a6eb2feb7c10",
+    "url": "/demo-routing/static/js/main.f79f3466.chunk.js"
+  },
+  {
+    "revision": "4b19246c99bb6f04cd6f",
+    "url": "/demo-routing/static/js/runtime-main.7c154ec0.js"
+  }
+]);
